7a440ed6d4a2b4efd333a51ac50ee8e90a3d9e04
krishna479 <krishankant.sharma@gmail.com>
2023-01-13 15:08:51 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
-public class Main {
+public class Main
+{
+    public static class Product{
 
+        public  int product(int a , int b){
+            return a*b;
+        }
+        public  int product(int a , int b, int c){
+            return a*b*c;
+        }
+        public  double product(double a , double b){
+            return a*b;
+        }
+    }
+    public static void main(String[] args) {
+        Product  p = new Product() ;
+        p.product(5,2);
+        p.product(5,2,3);
+        p.product(5.5,5.6);
+    }
 }
